e1e8577dc20d543e8d7b51ab5f8fc0494b982c00
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-05-16 16:35:25 +0000

    added table creation code to popover front end as well, to remove any php log warnings from network activated sites.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/popover@6196 f53fe46e-8c02-459b-9fdd-93e75771b278

--- popoverincludes/classes/popoverpublic.php
+++ popoverincludes/classes/popoverpublic.php
@@ -5,7 +5,7 @@ if(!class_exists('popoverpublic')) {
 	class popoverpublic {
 
 		var $mylocation = '';
-		var $build = 3;
+		var $build = 5;
 		var $db;
 
 		var $tables = array( 'popover', 'popover_ip_cache' );
@@ -31,10 +31,51 @@ if(!class_exists('popoverpublic')) {
 			$directories = explode(DIRECTORY_SEPARATOR,dirname(__FILE__));
 			$this->mylocation = $directories[count($directories)-1];
 
+			$installed = get_option('popover_installed', false);
+
+			if($installed === false || $installed != $this->build) {
+				$this->install();
+
+				update_option('popover_installed', $this->build);
+			}
+
 		}
 
 		function popoverpublic() {
 			$this->__construct();
+		}
+
+		function install() {
+
+			if($this->db->get_var( "SHOW TABLES LIKE '" . $this->popover . "' ") != $this->popover) {
+				 $sql = "CREATE TABLE `" . $this->popover . "` (
+				  	`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+					  `popover_title` varchar(250) DEFAULT NULL,
+					  `popover_content` text,
+					  `popover_settings` text,
+					  `popover_order` bigint(20) DEFAULT '0',
+					  `popover_active` int(11) DEFAULT '0',
+					  PRIMARY KEY (`id`)
+					)";
+
+				$this->db->query($sql);
+
+			}
+
+			// Add in IP cache table
+			if($this->db->get_var( "SHOW TABLES LIKE '" . $this->popover_ip_cache . "' ") != $this->popover_ip_cache) {
+				 $sql = "CREATE TABLE `" . $this->popover_ip_cache . "` (
+				  	`IP` varchar(12) NOT NULL DEFAULT '',
+					  `country` varchar(2) DEFAULT NULL,
+					  `cached` bigint(20) DEFAULT NULL,
+					  PRIMARY KEY (`IP`),
+					  KEY `cached` (`cached`)
+					)";
+
+				$this->db->query($sql);
+
+			}
+
 		}
 
 		function load_textdomain() {
