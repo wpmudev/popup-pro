90cce6aa97f74fb5f6062cca9ad1bb4e02088431
vladislav <vladislav@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-01-25 13:52:45 +0000

    Updated the authorship.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/popover@12653 f53fe46e-8c02-459b-9fdd-93e75771b278

--- popover.php
+++ popover.php
@@ -3,13 +3,14 @@
 Plugin Name: Popover plugin
 Plugin URI: http://premium.wpmudev.org/project/the-pop-over-plugin/
 Description: Allows you to display a fancy popup (powered as a popover!) to visitors sitewide or per blog, a *very* effective way of advertising a mailing list, special offer or running a plain old ad.
-Author: Barry (Incsub), Marko Miljus (Incsub)
+Author: WPMU DEV
 Version: 4.5.3
 Author URI: http://premium.wpmudev.org
 WDP ID: 123
 
 Copyright 2007-2013 Incsub (http://incsub.com)
-
+Author - Barry (Incsub)
+Contributors - Marko Miljus (Incsub), Ve Bailovity (Incsub)
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License (Version 2 - GPLv2) as published by
   the Free Software Foundation.
