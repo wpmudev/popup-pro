61e28c5dbd6b85387699a656a16a971a223178ff
Philipp Stracker <philipp@stracker.net>
2014-08-21 12:52:41 +0200

    - Display message about shortcodes for global PopUps.

--- views/settings.php
+++ views/settings.php
@@ -214,6 +214,17 @@ $ordered_rules = array();
 					'This list explains which shortcodes can be used with each ' .
 					'loading method:', PO_LANG
 				); ?>
+				<?php if ( IncPopup::use_global() ) : ?>
+					<p><em>
+					<?php _e(
+					'Important notice for shortcodes in <strong>Global ' .
+					'PopUps</strong>:<br />' .
+					'Shortcodes can be provided by a plugin or theme, so ' .
+					'each blog can have a different list of shortcodes. The ' .
+					'following list is valid for the current blog only!', PO_LANG
+					); ?>
+					</em></p>
+				<?php endif; ?>
 				<table class="widefat tbl-shortcodes load-<?php echo esc_attr( $cur_method ); ?>">
 					<thead>
 						<tr>
