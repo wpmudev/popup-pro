63c06d0bd257da79dd9195242822f291fb9f54aa
Philipp Stracker <stracker.phil@gmail.com>
2015-04-09 20:08:16 +0200

    Move list of available animations to the IncPopupBase class

--- views/meta-appearance.php
+++ views/meta-appearance.php
@@ -7,116 +7,7 @@
  */
 
 $styles = apply_filters( 'popup-styles', array() );
-
-$animations_in = array(
-	'' => array(
-		'' => __( '(No Animation)', PO_LANG ),
-	),
-	__( 'Attention Seekers', PO_LANG ) => array(
-		'bounce' => __( 'Bounce', PO_LANG ),
-		'flash' => __( 'Flash', PO_LANG ),
-		'pulse' => __( 'Pulse', PO_LANG ),
-		'rubberBand' => __( 'Rubber Band', PO_LANG ),
-		'shake' => __( 'Shake', PO_LANG ),
-		'swing' => __( 'Swing', PO_LANG ),
-		'tada' => __( 'Tada', PO_LANG ),
-		'wobble' => __( 'Wobble', PO_LANG ),
-	),
-	__( 'Bouncing Entrances', PO_LANG ) => array(
-		'bounceIn' => __( 'Bounce In', PO_LANG ),
-		'bounceInDown' => __( 'Bounce In Down', PO_LANG ),
-		'bounceInLeft' => __( 'Bounce In Left', PO_LANG ),
-		'bounceInRight' => __( 'Bounce In Right', PO_LANG ),
-		'bounceInUp' => __( 'Bounce In Up', PO_LANG ),
-	),
-	__( 'Fading Entrances', PO_LANG ) => array(
-		'fadeIn' => __( 'Fade In', PO_LANG ),
-		'fadeInDown' => __( 'Fade In Down', PO_LANG ),
-		'fadeInDownBig' => __( 'Fade In Down Big', PO_LANG ),
-		'fadeInLeft' => __( 'Fade In Left', PO_LANG ),
-		'fadeInLeftBig' => __( 'Fade In Left Big', PO_LANG ),
-		'fadeInRight' => __( 'Fade In Right', PO_LANG ),
-		'fadeInRightBig' => __( 'Fade In Right Big', PO_LANG ),
-		'fadeInUp' => __( 'Fade In Up', PO_LANG ),
-		'fadeInUpBig' => __( 'Fade In Up Big', PO_LANG ),
-	),
-	__( 'Flippers', PO_LANG ) => array(
-		'flip' => __( 'Flip', PO_LANG ),
-		'flipInX' => __( 'Flip In X', PO_LANG ),
-		'flipInY' => __( 'Flip In Y', PO_LANG ),
-	),
-	__( 'Lightspeed', PO_LANG ) => array(
-		'lightSpeedIn' => __( 'Light Speed In', PO_LANG ),
-	),
-	__( 'Rotating Entrances', PO_LANG ) => array(
-		'rotateIn' => __( 'Rotate In', PO_LANG ),
-		'rotateInDownLeft' => __( 'Rotate In Down Left', PO_LANG ),
-		'rotateInDownRight' => __( 'Rotate In Down Right', PO_LANG ),
-		'rotateInUpLeft' => __( 'Rotate In Up Left', PO_LANG ),
-		'rotateInUpRight' => __( 'Rotate In Up Right', PO_LANG ),
-	),
-	__( 'Specials', PO_LANG ) => array(
-		'rollIn' => __( 'Roll In', PO_LANG ),
-	),
-	__( 'Zoom Entrances', PO_LANG ) => array(
-		'zoomIn' => __( 'Zoom In', PO_LANG ),
-		'zoomInDown' => __( 'Zoom In Down', PO_LANG ),
-		'zoomInLeft' => __( 'Zoom In Left', PO_LANG ),
-		'zoomInRight' => __( 'Zoom In Right', PO_LANG ),
-		'zoomInUp' => __( 'Zoom In Up', PO_LANG ),
-	),
-);
-
-$animations_out = array(
-	'' => array(
-		'' => __( '(No Animation)', PO_LANG ),
-	),
-	__( 'Bouncing Exits', PO_LANG ) => array(
-		'bounceOut' => __( 'Bounce Out', PO_LANG ),
-		'bounceOutDown' => __( 'Bounce Out Down', PO_LANG ),
-		'bounceOutLeft' => __( 'Bounce Out Left', PO_LANG ),
-		'bounceOutRight' => __( 'Bounce Out Right', PO_LANG ),
-		'bounceOutUp' => __( 'Bounce Out Up', PO_LANG ),
-	),
-	__( 'Fading Exits', PO_LANG ) => array(
-		'fadeOut' => __( 'Fade Out', PO_LANG ),
-		'fadeOutDown' => __( 'Fade Out Down', PO_LANG ),
-		'fadeOutDownBig' => __( 'Fade Out Down Big', PO_LANG ),
-		'fadeOutLeft' => __( 'Fade Out Left', PO_LANG ),
-		'fadeOutLeftBig' => __( 'Fade Out Left Big', PO_LANG ),
-		'fadeOutRight' => __( 'Fade Out Right', PO_LANG ),
-		'fadeOutRightBig' => __( 'Fade Out Right Big', PO_LANG ),
-		'fadeOutUp' => __( 'Fade Out Up', PO_LANG ),
-		'fadeOutUpBig' => __( 'Fade Out Up Big', PO_LANG ),
-	),
-	__( 'Flippers', PO_LANG ) => array(
-		'flipOutX' => __( 'Flip Out X', PO_LANG ),
-		'flipOutY' => __( 'Flip Out Y', PO_LANG ),
-	),
-	__( 'Lightspeed', PO_LANG ) => array(
-		'lightSpeedOut' => __( 'Light Speed Out', PO_LANG ),
-	),
-	__( 'Rotating Exits', PO_LANG ) => array(
-		'rotateOut' => __( 'Rotate Out', PO_LANG ),
-		'rotateOutDownLeft' => __( 'Rotate Out Down Left', PO_LANG ),
-		'rotateOutDownRight' => __( 'Rotate Out Down Right', PO_LANG ),
-		'rotateOutUpLeft' => __( 'Rotate Out Up Left', PO_LANG ),
-		'rotateOutUpRight' => __( 'Rotate Out Up Right', PO_LANG ),
-	),
-	__( 'Specials', PO_LANG ) => array(
-		'hinge' => __( 'Hinge', PO_LANG ),
-		'rollOut' => __( 'Roll Out', PO_LANG ),
-	),
-	__( 'Zoom Exits', PO_LANG ) => array(
-		'zoomOut' => __( 'Zoom Out', PO_LANG ),
-		'zoomOutDown' => __( 'Zoom Out Down', PO_LANG ),
-		'zoomOutLeft' => __( 'Zoom Out Left', PO_LANG ),
-		'zoomOutRight' => __( 'Zoom Out Right', PO_LANG ),
-		'zoomOutUp' => __( 'Zoom Out Up', PO_LANG ),
-	),
-);
-
-
+$animations = IncPopup::get_animations();
 
 ?>
 <div class="wpmui-grid-12">
@@ -270,7 +161,7 @@ endif; ?>
 	</div>
 	<div class="col-6 inp-row">
 		<select id="po-animation-in" name="po_animation_in">
-			<?php foreach ( $animations_in as $group => $items ) : ?>
+			<?php foreach ( $animations->in as $group => $items ) : ?>
 				<?php if ( ! empty( $group ) ) : ?>
 				<optgroup label="<?php echo esc_attr( $group ); ?>">
 				<?php endif; ?>
@@ -291,7 +182,7 @@ endif; ?>
 
 	<div class="col-6 inp-row">
 		<select id="po-animation-out" name="po_animation_out">
-			<?php foreach ( $animations_out as $group => $items ) : ?>
+			<?php foreach ( $animations->out as $group => $items ) : ?>
 				<?php if ( ! empty( $group ) ) : ?>
 				<optgroup label="<?php echo esc_attr( $group ); ?>">
 				<?php endif; ?>
