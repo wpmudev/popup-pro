bd1a0e997d6800658125741ee2db9e3ad46f5bab
Philipp Stracker <stracker.phil@gmail.com>
2015-06-04 20:23:01 +0200

    Bump version to 4.7.0.8 and update WPMU Lib submodule

--- popover.php
+++ popover.php
@@ -3,7 +3,7 @@
 Plugin Name: PopUp Pro
 Plugin URI:  http://premium.wpmudev.org/project/the-pop-over-plugin/
 Description: Allows you to display a fancy PopUp to visitors sitewide or per blog. A *very* effective way of advertising a mailing list, special offer or running a plain old ad.
-Version:     4.7.0.7
+Version:     4.7.0.8
 Author:      WPMU DEV
 Author URI:  http://premium.wpmudev.org
 Textdomain:  popover
