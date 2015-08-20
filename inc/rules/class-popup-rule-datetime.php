56fa4f206158ae3932f36bb10eb7d5bff632d71f
Philipp Stracker <stracker.phil@gmail.com>
2015-08-20 17:43:15 +0200

    Add comment for compatibility with free version

--- inc/rules/class-popup-rule-datetime.php
+++ inc/rules/class-popup-rule-datetime.php
@@ -7,6 +7,7 @@ Author:      Philipp (Incsub)
 Author URI:  http://premium.wpmudev.org
 Type:        Rule
 Rules:       Show PopUp on Date, Hide PopUp on Date
+Limit:       pro
 Version:     1.0
 
 NOTE: DON'T RENAME THIS FILE!!
