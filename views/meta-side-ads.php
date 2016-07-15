3ac6d2d1c08ed196bb5e00e382bc54d3ebf6fbd4
Ricardo Freitas <hello@ricardotbfreitas.com>
2016-07-15 17:39:14 +0100

    Make strings ready for translate

--- views/meta-side-ads.php
+++ views/meta-side-ads.php
@@ -7,8 +7,8 @@
 
 ?>
 
-<p>You can now get PopUp Pro…for FREE!</p>
+<p><?php _e('You can now get PopUp Pro…for FREE!', 'popover'); ?></p>
 
-<p>No obligation, no contract, no catches. You’ll get PopUp Pro plus 100+ WPMU DEV plugins, Defender, Hummingbird & 24/7 WP support for absolutely nothing for 14 days.</p>
+<p><?php _e('No obligation, no contract, no catches. You’ll get PopUp Pro plus 100+ WPMU DEV plugins, Defender, Hummingbird & 24/7 WP support for absolutely nothing for 14 days.', 'popover'); ?></p>
 
-<a href="https://premium.wpmudev.org/project/the-pop-over-plugin/?utm_source=PopUp%20Free%20Plugin&utm_medium=Ad%20In%20PopUp%20Free%20&utm_content=Found%20Out%20More&utm_campaign=Get%20PopUp%20Pro" class="button-cta">Find Out More</a>
+<a href="https://premium.wpmudev.org/project/the-pop-over-plugin/?utm_source=PopUp%20Free%20Plugin&utm_medium=Ad%20In%20PopUp%20Free%20&utm_content=Found%20Out%20More&utm_campaign=Get%20PopUp%20Pro" class="button-cta"><?php _e('Find Out More', 'popover'); ?></a>
